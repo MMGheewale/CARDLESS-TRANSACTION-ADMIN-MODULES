46f8e63b6baf5c0d815d5562f4e28bf1c3542d29
mdmaazgheewale <101988656+mdmaazgheewale@users.noreply.github.com>
2022-05-15 14:06:25 +0200

    update

--- MODULE3/dbconnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $dbconn = mysql_connect('localhost','root',null);
-    mysql_select_db('money',$dbconn);
-    if(mysql_error()){
-        die ("Connection to database failed:  ".mysql_error());
-    }
-?>
